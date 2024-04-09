ecaf57927d69bf526806ec580db68ec81e0a5136
adiomi90 <adiomi90@outlook.com>
2024-04-10 00:24:33 +0300

    update on Advance task

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -52,6 +52,30 @@ def count_words(subreddit, word_list, after="", count=[]):
             for i in range(len(word_list)):
                 if (count[i] > 0) and i not in save:
                     print("{}: {}".format(word_list[i].lower(), count[i]))
-        else:
-    """_summary_
-    """            count_words(subreddit, word_list, after, count)
+        else:           
+
+
+count_words(subreddit, word_list, after, count)
+    else:
+        save = []
+        for i in range(len(word_list)):
+            for j in range(i + 1, len(word_list)):
+                if word_list[i].lower() == word_list[j].lower():
+                    save.append(j)
+                    count[i] += count[j]
+
+        for i in range(len(word_list)):
+            for j in range(i, len(word_list)):
+                if (count[j] > count[i] or
+                        (word_list[i] > word_list[j] and
+                         count[j] == count[i])):
+                    aux = count[i]
+                    count[i] = count[j]
+                    count[j] = aux
+                    aux = word_list[i]
+                    word_list[i] = word_list[j]
+                    word_list[j] = aux
+
+        for i in range(len(word_list)):
+            if (count[i] > 0) and i not in save:
+                print("{}: {}".format(word_list[i].lower(), count[i]))
